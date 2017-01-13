660e68cc8ba79d448fc7926ab95a7873f29e98d8
olivier <olivierloukombo@gmail.com>
2017-01-13 15:41:16 +0100

    deuxieme

---
+++ app/src/main/java/com/zack_olivier/popular_movies_stages1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zack_olivier.popular_movies_stages1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
